5cecca875a86dff403da5fa8a33946c20516973f
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-04-02 11:51:01 +0200

    changed navbar.php

--- src/navbar.php
+++ src/navbar.php
@@ -1,5 +1,28 @@
 <?php
 ?>
+
+<link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid"></div>
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                    <span class="mdi mdi-home">Home</span>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Link
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                </li>
+            </ul>
+        </div>
 </nav>
