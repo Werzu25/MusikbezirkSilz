8f9c4c5e6b927bcffe7d15f8f08c054d3bd844a5
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-04-02 12:16:33 +0200

    added imports for navbar.php and footer.php

--- src/navbar.php
+++ src/navbar.php
@@ -3,13 +3,15 @@
 
 <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<link href="../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
+
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarScroll">
             <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item">
-                    <span class="mdi mdi-home">Home</span>
+                    Bob lives in a <span class="mdi mdi-home"></span>.
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -21,7 +23,6 @@
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                </li>
                 </li>
             </ul>
         </div>
