4597f1c412e4f64729ae51055c42de426efada52
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-04-03 12:21:52 +0200

    fixed bug in footer.html

--- src/index.php
+++ src/index.php
@@ -1,3 +1,18 @@
+<link
+    href="../node_modules/bootstrap/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+/>
+<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<link
+    href="../node_modules/@mdi/font/css/materialdesignicons.min.css"
+    rel="stylesheet"
+/>
+
+<?php
+require_once "header.html";
+require_once "navbar.html";
+require_once "footer.html";
+?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -6,10 +21,7 @@
         <title>Document</title>
     </head>
     <body>
-        <?php
-        include "header.html";
-        include "navbar.html";
-        include "footer.html";
-        ?>
     </body>
 </html>
+<style>
+</style>
