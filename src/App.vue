9cfc676f5efa8c140c203ab61b515f08c9a779c3
junox0103 <julian.raffeiner@gmail.com>
2024-04-09 11:02:16 +0200

    elemen-foto-links hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import elementfotolinks from '@/components/templates/element-fotolinks.vue'
 import Footer from '@/components/Footer.vue'
 import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
   <main>
+    <elementfotolinks></elementfotolinks>
     <navbar></navbar>
   </main>
   <footer></footer>
