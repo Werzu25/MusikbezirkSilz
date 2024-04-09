231ed865c5dfd9ee7370f2ea26110317c020e662
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-04-09 10:36:55 +0200

    removed text

--- src/components/SceneBuilderWrapper.vue
+++ src/components/SceneBuilderWrapper.vue
@@ -1,10 +1,9 @@
 <script setup>
-import AFooter from '@/components/MainFooter.vue'
-import SceneBuilder from '@/components/SceneBuilder.vue'
+
 </script>
 
 <template>
-  <scene-builder></scene-builder>
+
 </template>
 
 <style scoped></style>
