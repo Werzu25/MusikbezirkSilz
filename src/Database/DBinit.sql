2428cc48451afd8522f3e98324829a8822e8cf6a
Peter <peteryt690@gmail.com>
2024-04-16 11:32:25 +0200

    Julian vergeilt

--- src/Database/DBinit.sql
+++ src/Database/DBinit.sql
@@ -1,14 +1,35 @@
-CREATE TABLE IF NOT EXISTS mainMenuEntry
+CREATE TABLE IF NOT EXISTS mainMenuEntry (
+    mainID INT NOT NULL AUTO_INCREMENT primary key,
+    name varchar(30),
+    order tinyint UNSIGNED
+);
 
-CREATE TABLE IF NOT EXISTS template
+CREATE TABLE IF NOT EXISTS template (
+    TemplateName varchar(60) NOT NULL primary key,
+);
 
-CREATE TABLE IF NOT EXISTS gallery
+CREATE TABLE IF NOT EXISTS gallery (
+    galleryID INT NOT NULL AUTO_INCREMENT primary key,
+    name varchar(255)
+);
 
-CREATE TABLE IF NOT EXISTS imgvid
+CREATE TABLE IF NOT EXISTS imgvid (
+    mainID INT NOT NULL AUTO_INCREMENT primary key,
+);
 
-CREATE TABLE IF NOT EXISTS subMenuEntry
+CREATE TABLE IF NOT EXISTS subMenuEntry (
+    mainID INT NOT NULL AUTO_INCREMENT primary key,
+    name
+    order tinyint UNSIGNED
+);
 
-CREATE TABLE IF NOT EXISTS entry
+CREATE TABLE IF NOT EXISTS entry (
+    mainID INT NOT NULL AUTO_INCREMENT primary key,
+    order tinyint UNSIGNED
+);
 
-CREATE TABLE IF NOT EXISTS galleryEntry
+CREATE TABLE IF NOT EXISTS galleryEntry (
+    mainID INT NOT NULL AUTO_INCREMENT primary key,
+    order tinyint UNSIGNED,
 
+);
