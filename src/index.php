2f8d30f6041d49d5ae622ac0c908b7d30d1960bd
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-16 10:43:29 +0200

    update

--- src/index.php
+++ src/index.php
@@ -1,12 +1,6 @@
-<link
-    href="../node_modules/bootstrap/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-/>
+<link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
 <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<link
-    href="../node_modules/@mdi/font/css/materialdesignicons.min.css"
-    rel="stylesheet"
-/>
+<link href="../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 
 <?php
 require_once "header.html";
@@ -15,13 +9,16 @@ require_once "footer.html";
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-    </head>
-    <body>
-    </body>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+</body>
+
 </html>
 <style>
 </style>
