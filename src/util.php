dab5b96526d7ae8e497a36d36a02234ccef294c8
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-23 10:43:28 +0200

    format code

--- src/util.php
+++ src/util.php
@@ -1,39 +1,39 @@
 <?php
 function fetchTable(string $table)
 {
-    $conn = new mysqli('localhost', getenv('USERNAME'), getenv('PASSWORD'), 'database');
+  $conn = new mysqli('localhost', getenv('USERNAME'), getenv('PASSWORD'), 'database');
 
-    if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
-    }
+  if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+  }
 
-    $sql = "SELECT * FROM $table";
-    $result = $conn->query($sql);
+  $sql = "SELECT * FROM $table";
+  $result = $conn->query($sql);
 
-    $tableData = [];
-    while ($row = $result->fetch_assoc()) {
-        $tableData[] = $row;
-    }
+  $tableData = [];
+  while ($row = $result->fetch_assoc()) {
+    $tableData[] = $row;
+  }
 
-    $conn->close();
-    return $tableData;
+  $conn->close();
+  return $tableData;
 }
 
 function customSelect(string $sql)
 {
-    $conn = new mysqli('localhost', getenv('USERNAME'), getenv('PASSWORD'), 'database');
+  $conn = new mysqli('localhost', getenv('USERNAME'), getenv('PASSWORD'), 'database');
 
-    if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
-    }
+  if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+  }
 
-    $result = $conn->query($sql);
+  $result = $conn->query($sql);
 
-    $tableData = [];
-    while ($row = $result->fetch_assoc()) {
-        $tableData[] = $row;
-    }
+  $tableData = [];
+  while ($row = $result->fetch_assoc()) {
+    $tableData[] = $row;
+  }
 
-    $conn->close();
-    return $tableData;
+  $conn->close();
+  return $tableData;
 }
