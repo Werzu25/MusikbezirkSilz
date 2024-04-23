977a3fa4958a4e263a0f8b2450d073d4998eed98
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-04-23 10:39:22 +0200

    Deleted unnecessary files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@mdi/font/css/materialdesignicons.css'
-import 'bootstrap'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
