4b65dff17d7b5b8599ddf6c4ae97ff9136a4a395
junox0103 <julian.raffeiner@gmail.com>
2024-04-30 11:28:44 +0200

    image carousel works now (petery gay)

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,12 +1,22 @@
-<div class="container w-50">
-  <div class="row">
-    <div class="col">
-      <img class="img-fluid w-25" alt="" src="../../assets/images/logo.jpg" />
+<link href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+<script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<link href="../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
+
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col">
+
+            <img class="img-fluid logoImage" alt="Musikbezirk Silz Logo" src="../../assets/images/Logo-MB-Silz-mini2.jpeg" />
+        </div>
+        <div class="col">
+            <p class="h1">Musikbezirk Silz</p>
+            <p class="fst-italic text-body-secondary">Es klingt Musik...</p>
+        </div>
     </div>
-    <div class="col">
-      <p class="h1">Musikbezirk Silz</p>
-      <p class="fst-italic text-body-secondary">Es klingt Musik...</p>
-    </div>
-  </div>
 </div>
-<style></style>
+<style>
+    .logoImage {
+        max-width: 25%;
+    }
+
+</style>
