299f5c6f4ae87135d3225b07e456599a31c61116
chris <christian.vor26@gmail.com>
2024-04-30 11:10:28 +0200

    fixed genContend, images still not working

--- src/genContent.php
+++ src/genContent.php
@@ -1,27 +1,45 @@
 <?php
 include 'util.php';
 include 'templates/header.php';
-include 'templates/navbar.php';
+//include 'templates/navbar.php';
 
 if (isset($_REQUEST['sideId'])) {
   $sideId = $_REQUEST['sideId'];
 } else {
   $sideId = 0;
+  header('Location: index.php');
 }
 
 $entrys = customSelect("SELECT * FROM entry WHERE entry.subID = $sideId");
 
+if(count($entrys) == 0){
+  header('Location: index.php');
+}
+$entrys = $entrys[0];
+
 $templateName = $entrys['TemplateName'];
 
+echo $templateName;
+
+
 switch ($templateName) {
-  case 'template1':
-    // renderTemplate1(TODO PARAMENTER EINFÜGEN);
+  case 'image-left':
+    $title = $entrys['title'];
+    $content = $entrys['text_entry'];
+    $picture = $entrys['picture'];
+    $time = /*$entrys['time']*/0;
+    include 'templates/image-left.php';
+
+    renderImageLeft($title, $content, $picture, $time);
     break;
-  case 'template2':
-    // renderTemplate2(TODO PARAMENTER EINFÜGEN);
-    break;
-  case 'template3':
-    // renderTemplate3(TODO PARAMENTER EINFÜGEN);
+  case 'image-right':
+    $title = $entrys['title'];
+    $content = $entrys['content'];
+    $picture = $entrys['picture'];
+    $time = /*$entrys['time']*/0;
+    include 'templates/image-right.php';
+
+    renderImageRight($title, $content, $picture, $time);
     break;
 }
 
