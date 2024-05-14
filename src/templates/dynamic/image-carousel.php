2ef08edd6b83f0e4da8901d3fd5f66ec1fe70d3e
junox0103 <julian.raffeiner@gmail.com>
2024-05-14 10:46:09 +0200

    fomrat (Peter gay)

--- src/templates/dynamic/image-carousel.php
+++ src/templates/dynamic/image-carousel.php
@@ -2,27 +2,33 @@
 <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <link href="../../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 <?php
-
-
 if (!function_exists('renderimaecarousel')) {
-    function renderimaecarousel($images)
-    {
-        echo '<div id="carouselExampleIndicators" class="carousel slide">
+  function renderimaecarousel($images)
+  {
+    echo '<div id="carouselExampleIndicators" class="carousel slide">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
-        for ($i = 1; $i < sizeof($images); $i++) {
-            echo ' <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="' . $i . '" aria-label="Slide ' . ($i + 1) . '"></button>';
-        }
-        echo '</div>
+    for ($i = 1; $i < sizeof($images); $i++) {
+      echo ' <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="' .
+        $i .
+        '" aria-label="Slide ' .
+        ($i + 1) .
+        '"></button>';
+    }
+    echo '</div>
     <div class="carousel-inner">';
-        $first = true;
-        foreach ($images as $image) {
-            echo ' <div class="carousel-item' . ($first ? ' active' : '') . '">
-            <img src="' . $image . '" class="d-block w-100" alt="...">
+    $first = true;
+    foreach ($images as $image) {
+      echo ' <div class="carousel-item' .
+        ($first ? ' active' : '') .
+        '">
+            <img src="' .
+        $image .
+        '" class="d-block w-100" alt="...">
         </div>';
-            $first = false;
-        };
-        echo '</div>
+      $first = false;
+    }
+    echo '</div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
@@ -32,8 +38,13 @@ if (!function_exists('renderimaecarousel')) {
         <span class="visually-hidden">Next</span>
     </button>
 </div>';
-    }
+  }
 }
 
-renderimaecarousel(['../../../assets/images/Logo-MB-Silz-mini2.jpeg', '../../../assets/images/Logo-MB-Silz-mini2.jpeg', '../../../assets/images/Logo-MB-Silz-mini2.jpeg']);
+renderimaecarousel([
+  '../../../assets/images/Logo-MB-Silz-mini2.jpeg',
+  '../../../assets/images/Logo-MB-Silz-mini2.jpeg',
+  '../../../assets/images/Logo-MB-Silz-mini2.jpeg',
+]);
+
 ?>
--- src/templates/dynamic/table.php
+++ src/templates/dynamic/table.php
@@ -1,34 +1,27 @@
 
-<?php
-
-
-if (!function_exists('renderTable')) {
-    function renderTable($tabletitle,$tableData)
-    {
-        echo ' <div class="container m-5 border-bottom-0">
+<?php if (!function_exists('renderTable')) {
+  function renderTable($tabletitle, $tableData)
+  {
+    echo ' <div class="container m-5 border-bottom-0">
 <table class="table table-bordered border-black text-center">
   <thead>';
-        for ($i = 0; $i < sizeof($tabletitle); $i++) {
-            echo '<th scope="col" style="width: 20%;">' . $tabletitle[$i] . '</th>';
-        }
-        echo '</thead>
+    for ($i = 0; $i < sizeof($tabletitle); $i++) {
+      echo '<th scope="col" style="width: 20%;">' . $tabletitle[$i] . '</th>';
+    }
+    echo '</thead>
   <tbody>
    <tr>';
-        for ($i = 0; $i < sizeof($tableData); $i++) {
-
-            foreach ($tableData[$i] as $value) {
-                echo '<td style="width: 20%;">' . $value . '</td>';
-            }
-            echo '</tr>';
-
-        }
-        echo '
+    for ($i = 0; $i < sizeof($tableData); $i++) {
+      foreach ($tableData[$i] as $value) {
+        echo '<td style="width: 20%;">' . $value . '</td>';
+      }
+      echo '</tr>';
+    }
+    echo '
   </tbody>
 </table>
 </div>';
-    }
+  }
 }
-
-
 
 ?>
