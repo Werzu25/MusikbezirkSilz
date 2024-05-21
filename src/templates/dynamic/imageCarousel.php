09b89e1c702a5b61899ceb62f655f3de11001848
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-05-21 10:04:27 +0200

    fixed imageCarousel.php

--- src/templates/dynamic/imageCarousel.php
+++ src/templates/dynamic/imageCarousel.php
@@ -1,6 +1,3 @@
-<link href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
-<script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<link href="../../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 <?php
 if (!function_exists('renderimaecarousel')) {
   function renderimaecarousel($images)
