74c8334c272c87ec7585e04815c9ea622f26b25f
junox0103 <julian.raffeiner@gmail.com>
2024-05-21 10:44:34 +0200

    header (peter gay)

--- src/templates/static/header.php
+++ src/templates/static/header.php
@@ -3,21 +3,27 @@
 <link href="../../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 
 <div class="container-fluid p-0">
+    <div class="container-fluid gruen " style="height: 20px; "></div>
     <div class="row row-cols-sm-6">
         <div class="col">
             <img class="img-fluid logoImage" alt="Musikbezirk Silz Logo" src="../assets/images/Logo-MB-Silz-mini2.jpeg" />
         </div>
-        <div class="col w-50">
+        <div class="col w-50 mt-auto mb-auto">
             <h1 class="head" >Musikbezirk Silz</h1>
             <p class="fst-italic text-body-secondary">Es klingt Musik...</p>
         </div>
     </div>
 <style>
+
     .logoImage {
         max-width: 30%;
         min-width: 125px;
     }
     .head {
         min-width: 600px;
+    }
+    .gruen{
+
+        background-color: #104b26;
     }
 </style>
