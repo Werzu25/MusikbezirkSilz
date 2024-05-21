2d9e69abc8360e4488df3ed3f98e9d126dd89799
raphael <rladinig@tsn.at>
2024-05-21 09:19:04 +0100

    ui update

--- src/templates/static/header.php
+++ src/templates/static/header.php
@@ -2,16 +2,10 @@
 <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <link href="../../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 
-<div class="container-fluid p-0">
-    <div class="row row-cols-sm-6">
-        <div class="col">
-            <img class="img-fluid logoImage" alt="Musikbezirk Silz Logo" src="../assets/images/Logo-MB-Silz-mini2.jpeg" />
-        </div>
-        <div class="col w-50">
+        <div class="col w-50 align-text-center m-2">
             <h1 class="head" >Musikbezirk Silz</h1>
             <p class="fst-italic text-body-secondary">Es klingt Musik...</p>
         </div>
-    </div>
 <style>
     .logoImage {
         max-width: 30%;
