d0f1d764c869b96ef5eb2ebd6e277809e365e7a1
Leon Wohlgemuth <lwohlgemuth@tsn.at>
2024-05-23 11:26:03 +0200

    updated scene-builder.php frontend

--- src/scene-builder.php
+++ src/scene-builder.php
@@ -12,8 +12,14 @@
   <div class="row h-100vh">
     <div class="spCol text-center" id="templateRenderer">
     test1
+      <div class="border rounded draggable">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+      <div class="border rounded draggable">
+        Raphi hat a skill issue
+      </div>
     </div>
-    <div class="verticalRuler h-100vh" id="pageDivider"></div>
+    <div class="verticalRuler h-100vh" id="pageDivider" ></div>
     <div class="spCol text-center" id="pagePreview">
       test
     </div>
@@ -29,7 +35,7 @@
     }
     .spCol {
         width: calc(50% - 1px);
-        min-width: 100px;
+        min-width: 200px;
     }
     .verticalRuler {
         width: 2px;
@@ -47,26 +53,79 @@
   let pagePreview = document.getElementById('pagePreview');
   let bodyWidth = document.body.clientWidth;
 
+  let draggable = document.querySelectorAll('.draggable');
+
+  draggable.forEach((element) => {
+    element.draggable = true;
+    element.addEventListener('dragstart',(e) => templateRendererDragStart(e));
+    element.id = Math.random().toString(36).substring(7);
+  });
+
+  pageDivider.addEventListener('dragover',(e) => preventDefault(e));
+  pagePreview.addEventListener('dragover', (e) => preventDefault(e));
+  pagePreview.addEventListener('drop', (e) => drop(e));
+
+  templateRenderer.addEventListener('dragover', (e) => preventDefault(e));
+  templateRenderer.addEventListener('drop', (e) => deleteElement(e));
+
+  function deleteElement(e) {
+    debugger
+    e.preventDefault();
+    let data = e.dataTransfer.getData("text");
+    document.getElementById(data).remove();
+  }
+
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+
+  function templateRendererDragStart(e) {
+    e.dataTransfer.setData("text", e.target.id);
+    e.dataTransfer.effectAllowed = "copy";
+  }
+
+  function pagePreviewDragStart(e) {
+    e.dataTransfer.setData("text", e.target.id);
+  }
+
+  function resetDragBehavior(element)  {
+    element.removeEventListener('dragstart',(e) => templateRendererDragStart(e));
+    element.addEventListener('dragstart',(e) => pagePreviewDragStart(e));
+  }
+
+  function drop(e) {
+    e.preventDefault();
+    let data = e.dataTransfer.getData("text");
+    let element = document.getElementById(data).cloneNode(true);
+    element.id = Math.random().toString(36).substring(7);
+    e.target.appendChild(element);
+    resetDragBehavior(element);
+  }
 
   function mouseDown(e) {
-    console.log('Dragging started');
     document.addEventListener("mousemove", mouseMove);
     document.addEventListener("mouseup", mouseUp);
+    pageDivider.style.userSelect = "none";
+    templateRenderer.style.userSelect = "none";
   }
 
   function mouseMove(e) {
     let x = e.clientX;
-    let min_width = templateRenderer.style.minWidth;
-    let leftWidth = Math.max(x - 1, min_width);
-    let rightWidth = Math.max(bodyWidth - x - 1, min_width);
+    let leftWidth = x - 1;
+    let rightWidth = bodyWidth - x - 1;
+    let minWidth = getComputedStyle(templateRenderer).getPropertyValue("min-width").replace("px", "");
+    if (leftWidth < minWidth || rightWidth < minWidth) {
+      return;
+    }
     templateRenderer.style.width = leftWidth + "px";
     pagePreview.style.width = rightWidth + "px";
   }
 
   function mouseUp(e) {
-    console.log('Dragging ended');
     document.removeEventListener("mousemove", mouseMove);
     document.removeEventListener("mouseup", mouseUp);
+    pageDivider.style.userSelect = "auto";
+    templateRenderer.style.userSelect = "auto";
   }
 
   pageDivider.addEventListener("mousedown", mouseDown);
