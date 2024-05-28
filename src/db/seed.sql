78a6522183e6a45dc45f5233877a6609af3f0c2e
Peter Quickner <98043962+DevilsBestFriend@users.noreply.github.com>
2024-05-28 15:02:23 +0200

    pm basic seed.sql geil

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -39,11 +39,11 @@ VALUES
         "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         2
     ),
-    (3, "title", "Test mit Leons Sis", 1),
+    (3, "title", "Sis", 1),
     (
         3,
         "text",
-        "Lorem ipsum dolor sit amet SEX mit Leons SCHWESTER, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        "Lorem ipsum dolor sit amet SCHWESTER, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         2
     ),
     (
@@ -55,12 +55,12 @@ VALUES
     (
         3,
         "link",
-        '{"href": "https://reintech.io/blog/php-json-encoding-decoding-web-services", "text": "penis"}',
+        '{"href": "https://reintech.io/blog/php-json-encoding-decoding-web-services", "text": "nicht"}',
         3
     ),
     (
         3,
         "table",
-        '{"titles": ["test", "test1", "leon"], "data": [["gay", "leon", ""], ["nox", "", ""], ["birgit", "", ""]]}',
+        '{"titles": ["test", "test1", "leon"], "data": [["abcd", "leon", "warum"], ["nox", "julian", "ju"], ["birgit", "komplett", "ja"]]}',
         4
     );
