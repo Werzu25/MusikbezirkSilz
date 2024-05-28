18df19c7f0c485e51effc5ef7659561de7f409f8
Peter Quickner <98043962+DevilsBestFriend@users.noreply.github.com>
2024-05-28 10:49:45 +0200

    navbar redone #findeich

--- src/templates/static/header.php
+++ src/templates/static/header.php
@@ -1,15 +1,10 @@
 <link href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
 <script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <link href="../../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
-<div class="container-fluid">
-    <div class="row imageRow">
-        <div class="col-lg-2">
-            <img class="img-fluid logoImage" alt="Musikbezirk Silz Logo" src="../assets/images/main-logo.png" />
-        </div>
-        <div class="col-lg-10 mt-auto">
-            <h1 class="head text">Musikbezirk Silz</h1>
-            <p class="fst-italic text">Es klingt Musik...</p>
-        </div>
+<div class="imageRow">
+    <div class="pt-2 pb-1 ps-5">
+        <h1 class="head text">Musikbezirk Silz</h1>
+        <p class="fst-italic text">Es klingt Musik...</p>
     </div>
 </div>
 <style>
@@ -23,6 +18,6 @@
         background: linear-gradient(45deg, white 20%, #003262 80%);
     }
     .text {
-        color: rgba(56, 57, 57, 0.75);
+        color: #484848;
     }
 </style>
