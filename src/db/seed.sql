59282e26a1ed74f415f71ed4f937403f78512d35
Peter Quickner <98043962+DevilsBestFriend@users.noreply.github.com>
2024-05-28 15:26:34 +0200

    haarglatzfall

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -36,7 +36,15 @@ VALUES
     (
         2,
         "text",
-        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        "Wenn du verschmieren mit Styling-Gel erwähnst, könnte es bedeuten, dass das Gel auf eine Weise aufgetragen wurde, die zu einem unordentlichen oder ungepflegten Aussehen führt. In der Regel wird Styling-Gel verwendet, um das Haar zu formen, zu definieren oder zu fixieren. Wenn es jedoch falsch angewendet wird oder zu viel verwendet wird, kann es dazu führen, dass das Haar fettig oder klebrig aussieht und sich nicht natürlich anfühlt.
+
+Einige mögliche Gründe für ein verschmiertes Aussehen könnten sein:
+
+    Übermäßige Verwendung: Zu viel Gel kann dazu führen, dass das Haar zusammenklebt und unordentlich aussieht, anstatt definiert und gestylt.
+    Falsche Anwendung: Wenn das Gel nicht gleichmäßig verteilt wird oder nicht richtig in das Haar eingearbeitet wird, kann es zu Klumpenbildung oder ungleichmäßigem Auftrag führen.
+    Zu schweres Gel: Manche Styling-Gele haben eine schwere Konsistenz, die das Haar beschweren kann, besonders bei feinem Haar. Dadurch kann das Haar flach und klebrig aussehen.
+
+Insgesamt ist es wichtig, die richtige Menge an Styling-Produkt zu verwenden und es gleichmäßig zu verteilen, um ein gepflegtes Aussehen zu erzielen, ohne dass das Haar "verschmiert" wirkt.",
         2
     ),
     (3, "title", "Sis", 1),
