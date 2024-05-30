104d4137b9c6df3c7030915d7d14af46b710d1a7
junox0103 <julian.raffeiner@gmail.com>
2024-05-30 17:12:04 +0200

    imageCarousel funktioniert !liegt an index! (Peter gay)

--- src/templates/dynamic/imageCarousel.php
+++ src/templates/dynamic/imageCarousel.php
@@ -1,8 +1,8 @@
-<link href="../../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
-<script src="../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<link href="../../../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 
 <?php
+
+
+
 
 
 function renderimageCarousel($images)
