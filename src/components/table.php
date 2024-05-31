02ca9b37bd8953cbb7ccad91f34b16d38cd0c8e5
raphael <rladinig@tsn.at>
2024-05-31 12:45:57 +0100

    fix: table

--- src/components/table.php
+++ src/components/table.php
@@ -5,8 +5,8 @@ function renderTable($table)
   echo ' <div class="container m-0 border-bottom-0">
 <table class="table table-bordered border-black text-center">
   <thead>';
-  for ($i = 0; $i < sizeof($table); $i++) {
-    echo '<th scope="col" style="width: 20%;">' . $table[0][$i] . '</th>';
+  foreach($table[0] as $title) {
+    echo '<th scope="col" style="width: 20%;">' . $title . '</th>';
   }
   echo '</thead>
   <tbody>
