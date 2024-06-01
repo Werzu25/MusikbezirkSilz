c9f6ea353a86cd6600fe9637c6c58d633ca8645c
Peter Q <peteryt690@gmail.com>
2024-06-01 15:17:30 +0200

    Das finde ich sehr hilfreich

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -1,22 +1,22 @@
 USE mbs;
-
 INSERT INTO
     mainMenuEntry (name, icon)
 VALUES
     ("Start", "mdi-home-variant"),
     ("Events", "mdi-calendar-multiple"),
-    ("Rückblick", ""),
-    ("Jugend", ""),
-    ("Über uns", "");
+    ("Rückblick", "mdi-reload"),
+    ("Jugend", "mdi-human-male-boy"),
+    ("Über uns", "mdi-chat-question-outline");
 
 INSERT INTO
     subMenuEntry (name, mmeID)
 VALUES
-    ("Bezirksmusikfest", 1),
-    ("Orchester Projekte", 2),
-    ("Jugend", 4),
-    ("Vorstand", 5),
-    ("Geschichte", 5);
+    ("DEFAULT_Start", 1),
+    ("Bezirksmusikfest", 2),
+    ("Orchester Projekte", 3),
+    ("Musikfeste", 3),
+    ("Ausbildung", 4),
+    ("Vorstand", 5);
 
 INSERT INTO
     articles (smeID)
@@ -24,54 +24,41 @@ VALUES
     (1),
     (2),
     (2),
+    (2),
     (3),
-    (4);
+    (4),
+    (5),
+    (6),
+    (7);
 
 INSERT INTO
     components (artID, type, content, displayOrder)
 VALUES
-    (1, "title", "Test", 1),
-    (
-        1,
-        "text",
-        
-	2
-    ),
-    (2, "title", "Test2", 1),
-    (
-        2,
-        "text",
-	"",
-        2
-    ),
-    (
-        1,
-        "carousel",
-        '["../assets/images/Eva.jpg", "../assets/images/Alfred.jpeg", "../assets/images/Ausschuss2018.JPG"]',
-        3
-    ),
-    (
-        3,
-        "link",
-        '{"href": "https://reintech.io/blog/php-json-encoding-decoding-web-services", "text": "Wir sind die Klotz Hannes Fangruppe"}',
-        3
-    ),
-    (
-        3,
-        "table",
-        '{"titles": ["test", "test1", "leon"], "data": [["abcd", "leon", "warum"], ["nox", "julian", "ju"], ["birgit", "komplett", "ja"]]}',
-        4
-    ),(3, "title", "Verschmiertes Haar", 1),
-    (
-        3,
-        "text",
-        "Verschmiertes Haar kann ein Zeichen für übermäßige Verwendung von Stylingprodukten sein. Es ist wichtig, die richtige Menge an Produkt zu verwenden und es gleichmäßig zu verteilen, um ein gepflegtes Aussehen zu erzielen.",
-        2
-    ),
-    (4, "title", "", 1),
-    (
-        4,
-        "text",
-        "",
-	2
-    );
+    (1, "title", "Start", 1),
+    (1, "text", "Yeil", 2),
+
+    (2, 'title', 'Bezirksmusikfest', 1),
+    (2, 'text', 'Hallo', 2),
+
+    (3, 'carousel', '["../assets/images/Eva.jpg", "../assets/images/Alfred.jpeg", "../assets/images/Ausschuss2018.JPG"]', 1),
+    (3, "link", '{"href": "https://bibel.github.io/", "text": "Datenschutzerklärung"}', 2),
+
+    (4, "table", '{"titles": ["Sölden", "27.07", "Tag des Klotz"], "data": [["6069", "9-8-2025", "Tag der Jungen & Jüngeren"], ["Sölden", "28.07.2024", "Bezirksmusikfest"], ["Haiming", "02.06", "Generanalversamlung"]]}', 1),
+    (4, "text", "angaben ohne gewähr", 2),
+
+    (5,'Title', 'Urheberrechtshinweis', 1),
+    (5, "link", '{"href": "https://bibel.github.io/EUe/ot/Gen_9.html", "text": "Datenschutzerklärung"}', 2),
+    (5, "text", 'Gen 9,16; Gilt besonders im Juni', 3),
+
+    (6, "title", "Es lebe die Musik", 1),
+    (6, "text", "Hallo", 2),
+
+    (7, "title", "Vorstand", 1),
+    (7, "text", "Hallo", 2),
+    
+    (8, 'title', 'Ich finde, dass ich finde, dass ich finde', 1),
+    (8, 'text', 'Ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde, dass ich finde', 2),
+
+    
+    (9, 'carousel', 'carousel', '["../assets/images/Eva.jpg", "../assets/images/Alfred.jpeg", "../assets/images/Ausschuss2018.JPG"], "../assets/images/Alfred.jpeg", "../assets/images/Alfred.jpeg"', 1),
+    (9, 'text', 'Unsere Ehrenmitglieder (Alfred ist cool)', 2);
