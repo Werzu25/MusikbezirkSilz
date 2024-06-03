27427068cdeed55066122f68b032384b7894d845
raphael <rladinig@tsn.at>
2024-06-03 19:56:30 +0100

    comments

--- src/db/init.sql
+++ src/db/init.sql
@@ -1,14 +1,14 @@
 DROP DATABASE IF EXISTS mbs;
 CREATE DATABASE IF NOT EXISTS mbs;
 USE mbs;
-
+-- leon falsche namen
 CREATE TABLE mainMenuEntry (
     mmeId INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(255),
     icon VARCHAR(255),
     PRIMARY KEY (mmeId)
 );
-CREATE TABLE subMenuEntry (
+CREATE TABLE subMenuEntry ( -- article
     smeId INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(255),
     mmeId INT,
@@ -16,7 +16,7 @@ CREATE TABLE subMenuEntry (
     FOREIGN KEY (mmeId) REFERENCES mainMenuEntry(mmeId)
 );
 
-CREATE TABLE articles (
+CREATE TABLE articles ( -- container
     artId INT AUTO_INCREMENT NOT NULL,
     creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     smeId INT,
@@ -24,7 +24,7 @@ CREATE TABLE articles (
     FOREIGN KEY (smeId) REFERENCES subMenuEntry(smeId)
 );
 
-CREATE TABLE components (
+CREATE TABLE components ( -- entry
     cmpId INT AUTO_INCREMENT NOT NULL,
     artId INT,
     type ENUM('text', 'title', 'table', 'link', 'carousel', 'media'),
