da753b67c77147c41c4d3911a69840fc38825056
Christian Vorhofer <christian.vor26@gmail.com>
2024-06-03 20:39:14 +0200

    login designed

--- src/login.php
+++ src/login.php
@@ -1,3 +1,6 @@
+<link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<link href="../node_modules/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
 
 <?php
 session_start();
