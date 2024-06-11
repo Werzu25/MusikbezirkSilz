d1bb255491f7aa16d0ee8ec38a107019aca69335
Peter Quickner <98043962+DevilsBestFriend@users.noreply.github.com>
2024-06-11 08:50:16 +0200

    funktioniert hoffentlich

--- src/insertApi.php
+++ src/insertApi.php
@@ -4,13 +4,14 @@ include 'util.php';
 $json = file_get_contents('php://input');
 if (!empty($json)) {
     $data = json_decode($json, true);
-    
+
     insertData($data);
 } else {
     echo "Keine Daten erhalten.";
 }
 
-function insertData($data) {
+function insertData($data)
+{
     $smmeId = $data['articleID'];
     $articles = json_decode($data['container'], true);
     $components = json_decode($articles['content'], true);
@@ -20,6 +21,15 @@ function insertData($data) {
     }
     foreach ($components as $component) {
         query("INSERT INTO components (articleId, type, content) VALUES (" . $component["artId"] . $component["type"] . ", " . $component["content"] . ")");
-    }   
+    }
 }
-?>
+
+// alle artikel mit componenten löschen
+function deleteSubpageArticles($smeID)
+{
+    $articleids = select("SELECT artId FROM components WHERE artId = $smeID");
+    foreach ($articleids as $articleid) {
+        query("DELETE FROM components WHERE artId = " . $articleid['artId']);
+    }
+    query("DELETE FROM articles WHERE smeId = $smeID");
+}
